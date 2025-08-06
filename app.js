2df40c515812dc013ee2ab3f741c437d6e533120
Juan David <juan-david.jdzb@hotmail.com>
2025-08-05 21:31:18 -0500

    Funcion agregar amigos

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
 let amigos = [];
+let nombreAmigo = "";
+
+function agregarAmigo() {
+    // Capturar el valor del campo de entrada
+    let nombreAmigo = document.querySelector('#amigo').value;
+    
+    // Validar la entrada
+    if (nombreAmigo != "") {
+        // Actualizar el array de amigos
+        amigos.push(nombreAmigo);
+    } else {
+        alert('Por favor, inserte un nombre.')
+    }
+
+    // Limpiar el campo de entrada
+    document.querySelector('#amigo').value = '';
+
+    return;
+}
